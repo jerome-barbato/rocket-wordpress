687c447b4da590b4068645e12b1b401ecb8df60a
jerome <jerome@metabolism.fr>
2017-01-26 08:20:16 -0800

    ..

--- src/Application.php
+++ src/Application.php
@@ -298,27 +298,8 @@ abstract class Application {
         if( $jpeg_quality = $this->config->get('jpeg_quality') )
             add_filter( 'jpeg_quality', create_function( '', 'return '.$jpeg_quality.';' ) );
 
-        if( $this->config->get('environement') != 'production' )
-            add_filter('gettext', array($this, 'force_register_gettext_strings'), 20, 3);
-
         //implement in src/application
         //ex : add_filter( 'page_link', array($this, 'rewrite_common'), 10, 3);
-    }
-
-
-    /**
-     * Force string registration for WPML using __('')
-     * @param $translated_text
-     * @param $text
-     * @param $domain
-     * @return mixed
-     */
-    function force_register_gettext_strings($translated_text, $text, $domain)
-    {
-        if( function_exists('icl_register_string') )
-            icl_register_string($domain, $text, $translated_text);
-
-        return $translated_text;
     }
 
 
