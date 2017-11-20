a9a82f6228ab5b3e0b037a6635d7efd4c8762a89
jerome <jerome@metabolism.fr>
2017-11-20 08:23:57 -0800

    updated Query model

--- doc/changelog.md
+++ doc/changelog.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/) 
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+# 1.2.31 - 2017-11-20
+### Added
+* Query::wp_query
+### Changed
+* Query::get_posts now use self::wp_query
+
 # 1.2.30 - 2017-11-17
 ### Added
 * Option to remove generated thumbnails, per site ( Settings > Media ) or globally ( Network Settings )
--- src/Model/Query.php
+++ src/Model/Query.php
@@ -22,25 +22,6 @@ class Query
 	}
 
 
-	public static function get_post($args=[], $fields=[])
-	{
-		if( empty($args) )
-			return new Post();
-
-		if( !is_array($args) )
-			return new Post($args);
-
-		$args['posts_per_page'] = 1;
-
-		$posts = self::get_posts($args, $fields);
-
-		if( count($posts) )
-			return $posts[0];
-
-		return $posts;
-	}
-
-
 	public static function get_terms($args=[])
 	{
 		$args['fields'] = 'ids';
@@ -110,7 +91,34 @@ class Query
 	}
 
 
+	public static function get_post($args=[], $fields=[])
+	{
+		if( empty($args) )
+			return new Post();
+
+		if( !is_array($args) )
+			return new Post($args);
+
+		$args['posts_per_page'] = 1;
+
+		$posts = self::get_posts($args, $fields);
+
+		if( count($posts) )
+			return $posts[0];
+
+		return $posts;
+	}
+
+
 	public static function get_posts($args=[], $fields=[])
+	{
+		$query = self::wp_query($args, $fields);
+
+		return $query->posts;
+	}
+
+
+	public static function wp_query($args=[], $fields=[])
 	{
 		global $wp_query;
 
@@ -121,10 +129,9 @@ class Query
 			$args['posts_per_page'] = get_option( 'posts_per_page' );
 
 		$args['fields'] = 'ids';
+		$query = new \WP_Query( $args );
 
-		$posts = get_posts( $args );
-
-		foreach ($posts as &$post)
+		foreach ($query->posts as &$post)
 		{
 			if( !empty($fields) )
 			{
@@ -148,6 +155,6 @@ class Query
 				$post = new Post( $post );
 		}
 
-		return $posts;
+		return $query;
 	}
 }
